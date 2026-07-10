a9d4ead1be20b899b27e8b8c9a83cf0546b21e70
JusTMeth25 <lorenzo.melis@yahoo.it>
2026-07-10 11:38:37 +0200

    Sistemati alcuni typo per cui non facevano visualizzare correttamente i file css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Search from "./pages/Search";
+import Detail from "./pages/Detail";
 
-function App() {}
+function App() {
+  return (
+    <div className="app">
+      <Navigation />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/detail/:city" element={<Detail />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
